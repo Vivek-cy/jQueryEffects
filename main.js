c7e9531d7bc6c392dff603fb20cf648b9b310266
Vivek Mandava <vivekmandava@Emma-2.local>
2022-11-29 07:39:39 -0500

    effects sample project

---
+++ main.js
@@ -0,0 +1,24 @@
+$(document).ready(() =>{
+    $('.hint-box').on('click', () =>{
+      $('.hint').slideToggle(500);
+    })
+    $('.wrong-answer-one').on('click', () =>{
+      $('.wrong-text-one').fadeOut('fast');
+      $('.frown').show();
+    })
+    $('.wrong-answer-two').on('click', () =>{
+      $('.wrong-text-two').fadeOut('fast');
+      $('.frown').show();
+    })
+    $('.wrong-answer-three').on('click', () =>{
+      $('.wrong-text-three').fadeOut('fast');
+      $('.frown').show();
+    })
+    $('.correct-answer').on('click', ()=>{
+      $('.frown').hide();
+      $('.smiley').show();
+      $('.wrong-text-one').hide();
+      $('.wrong-text-two').hide();
+      $('.wrong-text-three').hide();
+    })
+  });
